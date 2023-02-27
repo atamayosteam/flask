3cc53abde4f267fc8363a8876bc43c0442e0a0ad
Vlad Dracula <vlad@tran.sylvan.ia>
2023-02-27 10:51:59 -0500

    dd

---
+++ hello.py
@@ -0,0 +1,9 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("home.html.jinja", my_variable = "blue", my_list = "apple,pear,orange,bananna")
+
+
