744921d9e580ab798890614b669cbe15c41c960c
Vlad Dracula <vlad@tran.sylvan.ia>
2023-02-28 10:55:38 -0500

    n

---
+++ todo.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template, request
+
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("todo.html.jinja")
+
+my_list = ()
+
+@app.route("/add", methods=[POST])
+def add():
+    new_todo = request.form["new_todo"]
+    return new_todo
