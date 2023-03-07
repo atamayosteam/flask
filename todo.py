f98c73bf66e71f81e4e7782367a6a851578bb7b5
Vlad Dracula <vlad@tran.sylvan.ia>
2023-03-07 10:55:14 -0500

    x

--- todo.py
+++ todo.py
@@ -1,23 +1,39 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request,redirect
 
 
 app = Flask(__name__)
 
-my_todo = [
-    'Learn Tennis',
-    'Read more'
-] 
-
+bucketlist= ["skydiving","Ear-piercings"]
 
 @app.route("/")
 def index():
+    cursor = connection.cursor()
+    cursor.execute("SELECT * FROM `Todos`")
+
+    results = cursor.fetchall()
+
     return render_template(
         "todo.html.jinja",
-        todos = my_todo
-                           
-     )
-
+        bucketlist=results
+    )
 @app.route("/add", methods=['POST'])
 def add():
-    new_todo = request.form["new_todo"]
-    return new_todo
+    new_todo= request.form['new_todo']
+
+    bucketlist.append(new_todo)
+    return redirect(('/todo'))
+
+
+
+import pymysql
+import pymysql.cursors
+
+
+connection = pymysql.connect(
+    host = "10.100.33.60",
+    user = "atamayo",
+    password = "220886964",
+    database= "todos",
+    cursorclass=pymysql.cursors.DictCursor,
+    autocommit = True
+)
