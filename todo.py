4642fd179617cdf08fa68b696d3fd70dbba8987f
Vlad Dracula <vlad@tran.sylvan.ia>
2023-03-08 10:52:53 -0500

    n

--- todo.py
+++ todo.py
@@ -3,24 +3,24 @@ from flask import Flask, render_template, request,redirect
 
 app = Flask(__name__)
 
-bucketlist= ["skydiving","Ear-piercings"]
+todoss= ["read more","learn tennis"]
 
 @app.route("/")
 def index():
     cursor = connection.cursor()
-    cursor.execute("SELECT * FROM `Todos`")
+    cursor.execute("SELECT * FROM `todos`")
 
     results = cursor.fetchall()
 
     return render_template(
         "todo.html.jinja",
-        bucketlist=results
+        todoss=results
     )
 @app.route("/add", methods=['POST'])
 def add():
     new_todo= request.form['new_todo']
 
-    bucketlist.append(new_todo)
+    todoss.append(new_todo)
     return redirect(('/todo'))
 
 
@@ -33,7 +33,7 @@ connection = pymysql.connect(
     host = "10.100.33.60",
     user = "atamayo",
     password = "220886964",
-    database= "todos",
+    database="atamayo_flask",
     cursorclass=pymysql.cursors.DictCursor,
     autocommit = True
 )
