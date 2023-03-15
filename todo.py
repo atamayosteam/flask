4a3f1ee0c213afeb901678becd61997210fce099
Vlad Dracula <vlad@tran.sylvan.ia>
2023-03-15 10:54:47 -0400

    cdcd

--- todo.py
+++ todo.py
@@ -1,37 +1,54 @@
 from flask import Flask, render_template, request,redirect
+from flask_httpauth import HTTPBasicAuth
+from werkzeug.security import generate_password_hash, check_password_hash
+
 
 
 app = Flask(__name__)
+auth = HTTPBasicAuth()
+
+users = {
+    "Anthony": generate_password_hash("hello"),
+    "Tamayo": generate_password_hash("bye")
+}
+
+@auth.verify_password
+def verify_password(username, password):
+    if username in users and \
+            check_password_hash(users.get(username), password):
+        return username
 
 todos= ["read more","learn tennis"]
 
-@app.route("/")
+@app.route("/todo")
 def index():
     cursor = connection.cursor()
     cursor.execute("SELECT * FROM `todos`")
-
+    cursor.execute("SELECT * FROM `todos` ORDER BY `Complete`")
     results = cursor.fetchall()
 
     return render_template(
         "todo.html.jinja",
-        todoss=results
+        todos=results
     )
 @app.route("/add", methods=['POST'])
+@auth.login_required
 def add():
     new_todo= request.form['new_todo']
+    cursor = connection.cursor()
 
-    todos.append(new_todo)
-    return redirect(('/'))
+    return redirect(('/todo'))
 
-app.route("/complete", methods=["POST"])
+app.route("/Complete", methods=["POST"])
+@auth.login_required
 def complete():
     todo_id = request.form['todo_id']
 
     cursor= connection.cursor()
 
-    cursor.execute(f"UPDATE `todos` SET `complete` =1 WHERE `id` ={todo_id}")
+    cursor.execute(f"UPDATE `todos` SET `Complete` =1 WHERE `id` ={todo_id}")
 
-    return redirect("/")
+    return redirect("/todo")
 
 
 
